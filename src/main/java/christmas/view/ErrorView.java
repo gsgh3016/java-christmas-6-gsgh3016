441827abc5da36fb399f3c86707cdee6498b15a2
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-11 20:59:41 +0900

    feat: add error logger

---
+++ src/main/java/christmas/view/ErrorView.java
@@ -0,0 +1,13 @@
+package christmas.view;
+
+public class ErrorView {
+    private static final String ERROR = "[ERROR] ";
+
+    public static void printOnlyNumber() {
+        System.out.println(ERROR + "숫자만 입력해주세요.");
+    }
+
+    public static void printDateRange() {
+        System.out.println(ERROR + "유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+}
