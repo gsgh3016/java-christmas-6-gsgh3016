2b0f2496bea5948cfb5a4f1aab1a76e2a9e014ab
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-14 20:12:45 +0900

    test: complete DiscountManager test

---
+++ src/test/java/christmas/model/DiscountManagerTest.java
@@ -0,0 +1,19 @@
+package christmas.model;
+
+import christmas.util.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DiscountManagerTest {
+    @Test
+    void add_test() {
+        DiscountManager.add(Category.DISCOUNT, "크리스마스 디데이 할인: -1,200원");
+        DiscountManager.add(Category.DISCOUNT, "평일 할인: -4,046원");
+
+        assertThat(DiscountManager.getInstance().toString())
+                .contains(Category.DISCOUNT)
+                .contains("크리스마스 디데이 할인: -1,200원")
+                .contains("평일 할인: -4,046원");
+    }
+}
