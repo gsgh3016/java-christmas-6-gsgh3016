6b3210b8a45830c5d171abfccb3c15158af0cea2
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-14 22:26:39 +0900

    test: complete badge test

---
+++ src/test/java/christmas/util/BadgeTest.java
@@ -0,0 +1,42 @@
+package christmas.util;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BadgeTest {
+    enum STATE { NONE, STAR, TREE, SANTA }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(3_000, STATE.NONE),
+                Arguments.of(7_000, STATE.STAR),
+                Arguments.of(12_000, STATE.TREE),
+                Arguments.of(22_000, STATE.SANTA)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void select_test(int discount, STATE state) {
+        if (state == STATE.NONE) {
+            assertThat(Badge.select(discount)).isEqualTo(Badge.NONE);
+            return;
+        }
+        if (state == STATE.STAR) {
+            assertThat(Badge.select(discount)).isEqualTo(Badge.STAR);
+            return;
+        }
+        if (state == STATE.TREE) {
+            assertThat(Badge.select(discount)).isEqualTo(Badge.TREE);
+            return;
+        }
+        if (state == STATE.SANTA) {
+            assertThat(Badge.select(discount)).isEqualTo(Badge.SANTA);
+        }
+    }
+}
