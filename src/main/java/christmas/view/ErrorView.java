7944ed0403a6498871a17c9a13422e685b1171b8
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-14 13:44:48 +0900

    refactor: change input and error printing

--- src/main/java/christmas/view/ErrorView.java
+++ src/main/java/christmas/view/ErrorView.java
@@ -3,11 +3,7 @@ package christmas.view;
 public class ErrorView {
     private static final String ERROR = "[ERROR] ";
 
-    public static void printOnlyNumber() {
-        System.out.println(ERROR + "숫자만 입력해 주세요.");
-    }
-
-    public static void printDateRange() {
-        System.out.println(ERROR + "유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    public static void print(String message) {
+        System.out.println(ERROR + message);
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,21 +1,32 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.model.Menu;
 import christmas.model.MyDate;
-import christmas.util.Validator;
+import christmas.util.OrderParser;
 
-import java.time.format.DateTimeParseException;
+import java.util.EnumMap;
+
+import static christmas.util.DateValidator.checkDay;
+import static christmas.util.TotalOrderValidator.checkTotalOrder;
 
 public class InputView {
     public static MyDate inputDate() {
         while (true) {
             try {
-                String dateCommand = Validator.checkNumber(Console.readLine());
-                return new MyDate(dateCommand);
-            } catch (DateTimeParseException dateTimeParseException) {
-                ErrorView.printDateRange();
-            } catch (IllegalArgumentException illegalArgumentException) {
-                ErrorView.printOnlyNumber();
+                return new MyDate(checkDay(Console.readLine()));
+            } catch (Exception e) {
+                ErrorView.print(e.getMessage());
+            }
+        }
+    }
+
+    public static EnumMap<Menu, Integer> inputOrder() {
+        while (true) {
+            try {
+                return OrderParser.parse(checkTotalOrder(Console.readLine()));
+            } catch (Exception e) {
+                ErrorView.print(e.getMessage());
             }
         }
     }
