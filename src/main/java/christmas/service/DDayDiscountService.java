d445933921880ee0fdca91721db390903c15b42b
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-14 20:14:36 +0900

    feat: add d-day discount

---
+++ src/main/java/christmas/service/DDayDiscountService.java
@@ -0,0 +1,35 @@
+package christmas.service;
+
+import christmas.model.DiscountManager;
+import christmas.model.Menu;
+import christmas.model.MyDate;
+import christmas.util.Category;
+
+import java.util.EnumMap;
+import java.util.function.Predicate;
+
+public class DDayDiscountService extends DiscountService {
+    private static final String DDAY_DISCOUNT = "크리스마스 디데이 할인";
+
+    private static final int DAY_DISCOUNT_DEFAULT = 1_000;
+    private static final int DAY_OFFSET = 1;
+    private static final int DAY_DISCOUNT_UNIT = 100;
+
+    public DDayDiscountService(MyDate date, EnumMap<Menu, Integer> orders) {
+        super(date, orders);
+    }
+
+    public static int applyDiscount(int discountSum, MyDate date, Predicate<MyDate> condition, String discountLabel) {
+        if (condition.test(date)) {
+            int discountAmount = DAY_DISCOUNT_UNIT * (date.getDayOfMonth() - DAY_OFFSET);
+            discountSum += discountAmount;
+            String detail = String.format("%s: %d", discountLabel, discountAmount);
+            DiscountManager.add(Category.DISCOUNT, detail);
+        }
+        return DAY_DISCOUNT_DEFAULT + discountSum;
+    }
+
+    public static final DiscountStrategy DDAY_DISCOUNT_STRATEGY =
+            (discountSum, date) -> applyDiscount(discountSum, (MyDate) date, MyDate::isDDayPeriod, DDAY_DISCOUNT);;
+}
+
