9d9067233244bb1e182ee5d3f7f74844d7e2a44c
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-14 20:34:10 +0900

    test: complete day discount logic test

---
+++ src/test/java/christmas/service/DDayDiscountServiceTest.java
@@ -0,0 +1,32 @@
+package christmas.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DDayDiscountServiceTest {
+    private DDayDiscountService discountService;
+    private ServiceTestData testData;
+
+    private static final int DISCOUNT_UNIT = 100;
+    private static final int DISCOUNT_DEFAULT = 1_000;
+    private static final int DAY_OFFSET = 1;
+
+    @BeforeEach
+    void setUp() {
+        discountService = new DDayDiscountService(testData.date, testData.orders);
+        testData = new ServiceTestData();
+    }
+
+    @Test
+    void applyDDayDiscount_test() {
+        int discountSum = 0;
+        int expectedDiscount = DISCOUNT_DEFAULT + (testData.date.getDayOfMonth() - DAY_OFFSET) * DISCOUNT_UNIT;
+
+        discountSum = DDayDiscountService.DDAY_DISCOUNT_STRATEGY
+                .apply(discountSum, testData.date);
+
+        assertThat(discountSum).isEqualTo(expectedDiscount);
+    }
+}
---
+++ src/test/java/christmas/service/SingleOrderDiscountServiceTest.java
@@ -0,0 +1,48 @@
+package christmas.service;
+
+import christmas.model.Menu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SingleOrderDiscountServiceTest {
+    private SingleOrderDiscountService discountService;
+    private ServiceTestData testData;
+
+    private static final int DISCOUNT_UNIT = 2_023;
+
+    @BeforeEach
+    void setUp() {
+        discountService = new SingleOrderDiscountService(testData.date, testData.orders);
+        testData = new ServiceTestData();
+    }
+
+    @Test
+    void applyWeekDayDiscount_test() {
+        int discountSum = 0;
+        int expectedDiscount = (testData.orders.get(Menu.CHOCOLATE_CAKE)) * DISCOUNT_UNIT;
+
+        for (Map.Entry<Menu, Integer> order: testData.orders.entrySet()) {
+            discountSum = SingleOrderDiscountService.WEEKDAY_DISCOUNT_STRATEGY
+                    .apply(discountSum, order);
+        }
+
+        assertThat(discountSum).isEqualTo(expectedDiscount);
+    }
+
+    @Test
+    void applyWeekEndDiscount_test() {
+        int discountSum = 0;
+        int expectedDiscount = (testData.orders.get(Menu.BARBECUE_RIBS)) * DISCOUNT_UNIT;
+
+        for (Map.Entry<Menu, Integer> order: testData.orders.entrySet()) {
+            discountSum = SingleOrderDiscountService.WEEKEND_DISCOUNT_STRATEGY
+                    .apply(discountSum, order);
+        }
+
+        assertThat(discountSum).isEqualTo(expectedDiscount);
+    }
+}
