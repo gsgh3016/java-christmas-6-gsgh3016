07f4af94de65b66f203e6d93dcac797a389f56b4
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-15 18:04:02 +0900

    fix: add gift discount recording

--- src/main/java/christmas/service/GiftService.java
+++ src/main/java/christmas/service/GiftService.java
@@ -8,16 +8,24 @@ import christmas.util.constant.Formatting;
 public class GiftService {
     private static final int GIFT_POLICY = 120_000;
     private static final int NO_GIFT = 0;
+    private static final String GIFT_DISCOUNT = "증정 이벤트";
+
     private static final Menu gift = Menu.CHAMPAGNE;
-    private static final int quantity = 1;
+    private static final int GIFT_QUANTITY = 1;
 
     public int getGift(int totalPrice) {
         if (totalPrice >= GIFT_POLICY) {
-            String formattedGift = String.format(Formatting.MENU, gift.getName(), quantity);
-            DiscountManager.add(Category.GIFT, formattedGift);
-            return gift.getPrice() * quantity;
+            recordGift();
+            return gift.getPrice() * GIFT_QUANTITY;
         }
         DiscountManager.add(Category.GIFT, Category.NO);
         return NO_GIFT;
+    }
+
+    private static void recordGift() {
+        String formattedGift = String.format(Formatting.MENU, gift.getName(), GIFT_QUANTITY);
+        DiscountManager.add(Category.GIFT, formattedGift);
+        String formattedGiftPrice = String.format(Formatting.DISCOUNT, GIFT_DISCOUNT, -gift.getPrice() * GIFT_QUANTITY);
+        DiscountManager.add(Category.DISCOUNT, formattedGiftPrice);
     }
 }
